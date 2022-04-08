ddf197065895cf70163592fd041d3876d740bf9f
Stealth2476 <99193141+Stealth2476@users.noreply.github.com>
2022-04-08 10:09:07 -0400

    Add files via upload

---
+++ processDNS_FilterFile.ps1
@@ -0,0 +1,142 @@
+# MIT License
+
+# Copyright (c) 2022 Unisys, inc.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+#==============================================================================
+# 
+#               comand line parameter -filterfile <path to input csv file>
+#               format of input CSV file:
+#   Object,Function,Name,Description,Type,IP,Protocol,LocalPort,RemotePort,ExLocal,ExRemote,Exclude,Qualifier,COIFilter
+#               
+#               Example input file:
+#   Object,Function,Name,Description,Type,IP,Protocol,LocalPort,RemotePort,ExLocal,ExRemote,Exclude,Qualifier,COIFilter
+    # FilterList,Use,FL AD Server IP,List of AD Server IPs,,,,,,,,,,
+    # FilterList,Attach_Qual,,www.cnn.com,,151.101.1.67,,,,,,,,,
+    # FilterList,Attach_Qual,,www.cnn.com,,151.101.129.67,,,,,,,,,
+    # FilterList,Attach_Qual,,www.cnn.com,,151.101.193.67,,,,,,,,,
+    # FilterList,Attach_Qual,,www.cnn.com,,151.101.65.67,,,,,,,,,
+    # FilterList,Attach_Qual,,www.cnbc.com,,96.6.22.232,,,,,,,,,
+
+# after the running of the script two files will be created 
+#           1)  <input file name>-newConfig.csv is the new configuration file
+#                that needs to be run through the Stealth ecoAPI generator 
+#           2)  <input file name>-previous.csv This file is the configuration 
+#               that was generated from the previous run and is compared to the current run
+#==============================================================================
+param ($filterfile)
+
+$configuration = @{}
+
+function read-FilterFile{
+    param ($filterFileName)
+    get-content $filterFileName
+}
+
+function readPreviousConfig{
+    param ($filename)
+    get-content $filename
+}
+
+function buildNewFilterList
+{
+    param ($filterFile,$configuration)
+    $newFilterList =@()
+
+    foreach ($rec in $filterFile)
+        {
+            $cells = $rec.split(",")
+            switch ($cells[1])
+                {
+                    "Attach_Qual"{
+                        $url=$cells[3].Trim()
+                        try {
+                            $dns= [System.Net.Dns]::GetHostEntry($url).AddressList.IPAddressToString
+                            $IPAs = $dns | Sort-Object
+                            foreach ($Arec in $IPAs)
+                            {
+                                $cells[5]=$Arec
+                                $newRec=""
+                                foreach ($c in $cells)
+                                    {
+                                        $newRec=$newRec+$c+","
+                                    }
+                                $newFilterList += $newRec
+                            }
+                        } catch {
+                            $newFilterList += $rec
+                        }
+                    }
+                    Default {
+                        $newFilterList += $rec
+                    }
+                }
+        }
+    $newFilterList
+
+}
+
+#==============================================================================
+if ($null -eq $filterfile)
+{
+    Write-Host "No filter file supplied, exiting..."
+    $filterfile =".\test.csv"
+}
+
+if (-not(Test-Path $filterfile)){
+    Write-Host "config file not found"
+    exit 
+}
+
+$previousConfigFileName = $filterfile.substring(0,$filterfile.indexof(".csv"))+"-previous.csv"
+
+$configFile=read-FilterFile $filterfile
+
+if (Test-Path $previousConfigFileName){
+    $previousConfig =  readPreviousConfig $previousConfigFileName
+} else {
+    Write-Host "Previous configuration not found"
+}
+        
+$fl1 = buildNewFilterList -filterFile $configFile -configuration $configuration
+$fl1 | Out-File -FilePath $previousConfigFileName -Encoding ASCII 
+$tmpConfig=Compare-Object  -ReferenceObject $previousConfig  -DifferenceObject $fl1 -IncludeEqual
+
+
+if (Test-Path $filterFile"-newConfig.csv" ){
+    Remove-Item $filterFile"-newConfig.csv"
+}
+
+$recNo=0
+$newconfig=@()
+foreach ($Iobj in $tmpConfig.GetEnumerator())
+{
+    $rec=$Iobj.InputObject
+    $diff=$Iobj.SideIndicator
+    $recNo +=1
+    if ($recNo -gt 3){
+        if ($diff.contains("<=")) {
+            $newconfig += $rec.replace("Attach_Qual","Detach_Qual")
+        } else {
+            $newconfig += $rec
+        }
+        $rec | Out-File -FilePath $filterFile"-newConfig.csv" -Encoding ASCII -Append
+    }
+}
